3e9ca88e59ee44bf852b45bbc248d317f331f862
Jayson Virissimo <jayson.virissimo@asu.edu>
2017-06-25 12:23:50 -0700

    Get and display badges via AJAX

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
-  // your code will go here
-
+  var reportCardUrl = 'https://www.codeschool.com/users/Jayson_Virissimo.json';
+  $.ajax({
+    url: reportCardUrl,
+    dataType: 'jsonp',
+    success: function(response) {
+      var courses = response.courses.completed;
+      var length = courses.length;
+      for (var i = 0; i < length; i++) {
+        var element = '<div class="course">';
+        element += '<h3>' + courses[i].title + '</h3>';
+        element += '<img src="' + courses[i].badge + '"></img>';
+        element += '<a href="' + courses[i].url + '" target="blank" class="btn btn-primary">See Courses</a>';
+        element += '</div>';
+        $('#badges').append(element);
+      }
+    }
+  });
 });
